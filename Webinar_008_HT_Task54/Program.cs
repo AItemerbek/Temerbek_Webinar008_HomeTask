bc7c650f797eb322a19a948a0917933948836f96
alekseytemerbek <tech.temerbek@gmail.com>
2023-09-11 15:17:53 +0300

    add HomeTask 54

---
+++ Webinar_008_HT_Task54/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] array, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(minValue, maxValue + 1);
+}
+
+void SortRowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int min = array[i,0];
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    min = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = min;
+                }
+            }
+        }
+    }
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+
+int rows = InputNum("Введите количество строк: ");
+int cols = InputNum("Введите количество столбцов: ");
+int min = InputNum("Введите минимальное значение диапазона: ");
+int max = InputNum("Введите максимальное значение диапазона: ");
+
+int[,] anyArray = Create2DArray(rows, cols);
+Fill2DArray(anyArray, min, max);
+Print2DArray(anyArray);
+Console.WriteLine();
+SortRowArray(anyArray);
+Print2DArray(anyArray);
